cb21b9fdebbc2b48945d858b751274fe15ebfcc8
Thanut Panichyotai <luvikung@gmail.com>
2024-05-30 18:16:37 +0700

    move file.

--- Expection/NBTCompoundInvalidKeyFormatException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NBT.Exceptions
-{
-    public sealed class NBTCompoundInvalidKeyFormatException : NBTException
-    {
-        private NBTCompoundInvalidKeyFormatException() { }
-        public NBTCompoundInvalidKeyFormatException(string? key) : base(key is null ? $"Key is null." : $"Key is invalid format. The value is \'{key}\'") { }
-    }
-}
--- Expection/NBTReaderInvalidFormatException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NBT.Exceptions
-{
-    public class NBTReaderInvalidFormatException : NBTException
-    {
-        private NBTReaderInvalidFormatException() { }
-        public NBTReaderInvalidFormatException(Type readerType, char c) : base($"Cannot read the character \'{c}\' in reader of {nameof(readerType)}") { }
-    }
-}
